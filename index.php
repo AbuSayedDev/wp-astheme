8b81fcc679cd58a170e221be45e27427ee5f6ea1
Abu Sayed <abusayed285235@gmail.com>
2023-12-20 21:33:31 +0600

    charset dynamic

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <!DOCTYPE html>
 <html lang="<?php language_attributes(); ?>" class="no-js">
 <head>
-    <meta charset="<?php ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
