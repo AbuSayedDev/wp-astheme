1bacf8923327d3e97a10e825179601243010509e
Abu Sayed <abusayed285235@gmail.com>
2023-12-21 01:23:18 +0600

    functions file create and title dynamic

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <?php wp_head(); ?>
 </head>
+<body <?php body_class(); ?> >
 
-<body>
 
+<?php wp_footer(); ?>
 </body>
-
 </html>
+
