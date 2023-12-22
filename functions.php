8e87df4e7df145800c4fdd6603180b99e67e0042
Abu Sayed <abusayed285235@gmail.com>
2023-12-22 23:09:57 +0600

    wp customize header logo dynamic

--- functions.php
+++ functions.php
@@ -8,7 +8,6 @@
 add_theme_support('title-tag');
 
 //Theme CSS and JQuery File calling
-
 function astheme_css_js_file_calling(){
 
     // wp enqueue style
@@ -22,3 +21,26 @@ function astheme_css_js_file_calling(){
     wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.js', array(), '5.0.2', 'true');
 }
 add_action('wp_enqueue_scripts', 'astheme_css_js_file_calling');
+
+
+// Theme Function
+function astheme_customizar_register($we_customize){
+    $we_customize->add_section('astheme_header_area', array(
+        'title'       => __('Header Area', 'astheme'),
+        'description' => 'If you interested to update your header area, you can do it here.'
+    ));
+
+    $we_customize->add_setting('astheme_logo', array(
+        'default' =>  get_bloginfo( 'template_directory' ) . '/assets/img/Logo.png'
+    ));
+
+    $we_customize->add_control(new WP_Customize_Image_Control($we_customize, 'astheme_logo', array(
+        'label' => 'Logo Upload',
+        'description' => 'If you interested to change or update your logo you can do it.',
+        'section' => "astheme_header_area",
+        'setting' => 'astheme_logo'
+
+    ) ));
+}
+add_action( 'customize_register', 'astheme_customizar_register');
+
--- index.php
+++ index.php
@@ -14,6 +14,49 @@
 </head>
 <body <?php body_class(); ?> >
 
+<div class="header-area bg-light">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light py-3">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">
+                            <img src="<?php echo get_theme_mod( 'astheme_logo' ); ?>" alt="logo" width="120px" height="auto">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                            <ul class="navbar-nav mb-2 mb-lg-0 ">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">About Us</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Services
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php wp_footer(); ?>
 </body>
