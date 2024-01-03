29d4b79e64efec53304f3cb7e6c2d05104e67510
Abu Sayed <abusayed285235@gmail.com>
2024-01-04 01:47:42 +0600

    menu position add

--- index.php
+++ index.php
@@ -18,47 +18,29 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light py-3">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">
-                            <img src="<?php echo get_theme_mod( 'astheme_logo' ); ?>" alt="logo" width="120px" height="auto">
-                        </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-
-                        <?php wp_nav_menu( array(
-                            'theme_location' => 'primary_menu',
-                            'container' => false,
-                            'menu_class' => '',
-                            'fallback_cb' => '__return_false',
-                            'items_wrap' => '<ul id="bootstrap-navbar" class="navbar-nav mb-2 mb-lg-0 %2$s">%3$s</ul>',
-                            'depth' => 2,
-                            'walker' => new bootstrap_5_wp_nav_menu_walker()
-                        ) ); ?>
-                            <!-- <ul class="navbar-nav mb-2 mb-lg-0 ">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">About Us</a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Services
+                <nav id="navarea" class="navbar navbar-expand-lg navbar-light bg-light py-3 <?php echo get_theme_mod('astheme_menu_position'); ?>" >
+                    <div class="container-fluid row">
+                            <div class="col-6 col-md-3">
+                                <a class="navbar-brand" href="#">
+                                    <img src="<?php echo get_theme_mod( 'astheme_logo' ); ?>" alt="logo" width="120px" height="auto">
                                 </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Contact Us</a>
-                                </li>
-                            </ul> -->
+                            </div>
+                        
+                            <div class="col-6  col-md-9">
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                                <?php wp_nav_menu( array(
+                                    'theme_location' => 'primary_menu',
+                                    'container' => false,
+                                    'menu_class' => '',
+                                    'fallback_cb' => '__return_false',
+                                    'items_wrap' => '<ul id="bootstrap-navbar" class="navbar-nav mb-2 mb-lg-0 %2$s">%3$s</ul>',
+                                    'depth' => 2,
+                                    'walker' => new bootstrap_5_wp_nav_menu_walker()
+                                ) ); ?>
+                            </div>
                         </div>
                     </div>
                 </nav>
